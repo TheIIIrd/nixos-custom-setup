c70c3354aa459577c507f62b9679fab488aaa93d
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-01-09 14:48:49 +0300

    Update nix-conf-plus.nix

--- nix-conf-plus.nix
+++ nix-conf-plus.nix
@@ -18,7 +18,7 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ]
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
