dd061e8edd45cc97edfb22365d940198d22a67b7
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-01-09 08:37:43 +0300

    Update nix-conf-vm.nix

--- nix-conf-vm.nix
+++ nix-conf-vm.nix
@@ -68,76 +68,25 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
     # If you want to use JACK applications, uncomment this
-    # jack.enable = true;
+    #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    # media-session.enable = true;
+    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # # Enable OpenGL
-  # hardware.opengl = {
-  #   enable = true;
-  #   driSupport = true;
-  #   driSupport32Bit = true;
-  # };
-
-  # # Load nvidia driver for Xorg and Wayland
-  # services.xserver.videoDrivers = ["nvidia"];
-
-  # hardware.nvidia = {
-
-  #   # Modesetting is required.
-  #   modesetting.enable = true;
-
-  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-  #   powerManagement.enable = false;
-  #   # Fine-grained power management. Turns off GPU when not in use.
-  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #   powerManagement.finegrained = false;
-
-  #   # Use the NVidia open source kernel module (not to be confused with the
-  #   # independent third-party "nouveau" open source driver).
-  #   # Support is limited to the Turing and later architectures. Full list of 
-  #   # supported GPUs is at: 
-  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-  #   # Only available from driver 515.43.04+
-  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
-  #   open = false;
-
-  #   # Enable the Nvidia settings menu,
-	#   # accessible via `nvidia-settings`.
-  #   nvidiaSettings = true;
-
-  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
-
-  # hardware.nvidia.prime = {
-  #   sync.enable = true;
-
-  #   # Make sure to use the correct Bus ID values for your system!
-  #   # Use sudo lshw -c display
-  #   nvidiaBusId = "PCI:14:0:0";
-  #   intelBusId = "PCI:0:2:0";
-  # };
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.theiiird = {
     isNormalUser = true;
     description = "theiiird";
-    extraGroups = [
-      "networkmanager"
-      "libvirtd"
-      "wheel"
-    ];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
     packages = with pkgs; [
       firefox
+      floorp
       kate
     ];
   };
@@ -222,17 +171,8 @@
   # };
 
   # List services that you want to enable:
-  # services.auto-cpufreq.enable = true;
-  # services.auto-cpufreq.settings = {
-  #   battery = {
-  #    governor = "powersave";
-  #    turbo = "never";
-  #   };
-  #   charger = {
-  #    governor = "performance";
-  #    turbo = "auto";
-  #   };
-  # };
+  services.spice-vdagentd.enable = true;
+  services.qemuGuest.enable = true;
 
   services.zerotierone.enable = true;
   services.flatpak.enable = true;
@@ -247,14 +187,11 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
-  services.spice-vdagentd.enable = true;
-  services.qemuGuest.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
   # Enable flakes
   # nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
